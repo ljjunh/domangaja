284b8803c987c1025063e803922443ab3abe019a
ljjunh <limjunhee4575@gmail.com>
2026-06-14 22:44:34 +0900

    chore: tanstackQuery setting

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Navigation } from '@/shared/navigations/index';
+import { queryClient } from '@/shared/apis/queryClient';
 
 function App() {
   return (
     <SafeAreaProvider>
-      <Navigation />
+      <QueryClientProvider client={queryClient}>
+        <Navigation />
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
---
+++ src/shared/apis/queryClient.ts
@@ -0,0 +1,15 @@
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      refetchOnWindowFocus: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
