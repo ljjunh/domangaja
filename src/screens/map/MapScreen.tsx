f4b67155459a2319a701f77708b91133b8735e40
ljjunh <limjunhee4575@gmail.com>
2026-07-17 17:40:54 +0900

    feat: 메인탭 스크린 스크롤뷰 레이아웃 설정

--- src/screens/map/MapScreen.tsx
+++ src/screens/map/MapScreen.tsx
@@ -1,5 +1,23 @@
-import { Text } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Text } from '@/shared/components/base';
+import { Layout } from '@/shared/components/layout';
+import { MAIN_TAB_SCREEN_EDGES, SCREEN_PADDING_HORIZONTAL } from '@/shared/constants/layout';
+import { useMainTabBarSpace } from '@/shared/hooks/useMainTabBarSpace';
 
 export default function MapScreen() {
-  return <Text>MapScreen</Text>;
+  const mainTabBarSpace = useMainTabBarSpace();
+
+  return (
+    <Layout edges={MAIN_TAB_SCREEN_EDGES}>
+      <ScrollView contentContainerStyle={[styles.container, { paddingBottom: mainTabBarSpace }]}>
+        <Text typography="t1">Map Screen</Text>
+      </ScrollView>
+    </Layout>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: SCREEN_PADDING_HORIZONTAL,
+  },
+});
