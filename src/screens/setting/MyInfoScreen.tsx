a0af585df103f610d4ca391cb75fe28b95340e86
ljjunh <limjunhee4575@gmail.com>
2026-07-21 14:29:17 +0900

    chore: image-crop-picker setting

--- src/screens/setting/MyInfoScreen.tsx
+++ src/screens/setting/MyInfoScreen.tsx
@@ -1,10 +1,21 @@
-import { View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import { Text } from '@/shared/components/base';
+import { Layout, StackHeader } from '@/shared/components/layout';
+import { SCREEN_PADDING_HORIZONTAL } from '@/shared/constants/layout';
 
 export default function MyInfoScreen() {
   return (
-    <View>
-      <Text typography="t1">MyInfoScreen</Text>
-    </View>
+    <Layout>
+      <StackHeader title="내 정보" />
+      <ScrollView style={styles.container}>
+        <Text typography="t1">MyInfoScreen</Text>
+      </ScrollView>
+    </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: SCREEN_PADDING_HORIZONTAL,
+  },
+});
