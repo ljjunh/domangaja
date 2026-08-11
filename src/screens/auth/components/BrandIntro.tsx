a32473856a935cf9dc1e1b6f82c5783bbd21b218
ljjunh <limjunhee4575@gmail.com>
2026-08-11 19:03:55 +0900

    feat: 스플래시 스크린, 앱 아이콘

--- src/screens/auth/components/BrandIntro.tsx
+++ src/screens/auth/components/BrandIntro.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, View } from 'react-native';
 import { Text } from '@/shared/components/base';
-import { colors } from '@/shared/constants/colors';
 import { useTranslation } from 'react-i18next';
+import { AppIcon } from '@/assets/logo';
 
 export default function BrandIntro() {
   const { t } = useTranslation();
 
   return (
     <View style={styles.container}>
-      <View style={styles.logo} />
+      <AppIcon width={120} height={120} />
       <View style={styles.textGroup}>
         <Text typography="t1" weight="bold" textAlign="center">
           {t('login.title')}
@@ -22,13 +22,7 @@ export default function BrandIntro() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', gap: 20 },
-  logo: {
-    width: 82,
-    height: 82,
-    backgroundColor: colors.blue[500],
-    borderRadius: 12,
-  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   textGroup: {
     gap: 10,
     justifyContent: 'center',
