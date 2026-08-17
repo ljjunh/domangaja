c8f4cd13706e2202e11791685be8fe7ef7ca5253
ljjunh <limjunhee4575@gmail.com>
2026-08-17 12:31:09 +0900

    feat: 저장한 곳 조회 api

--- src/screens/setting/components/index.ts
+++ src/screens/setting/components/index.ts
@@ -1,6 +1,7 @@
 export { default as LanguageListItem } from './LanguageListItem';
 export { default as MyInfoForm } from './MyInfoForm';
 export { default as MyInfoSkeleton } from './MyInfoSkeleton';
+export { default as ScrapSkeleton } from './ScrapSkeleton';
 export { default as SettingListItem } from './SettingListItem';
 export { default as SettingSection } from './SettingSection';
 export { default as SettingToggleItem } from './SettingToggleItem';
